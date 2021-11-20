909da7a1e18791237967ccea18b9aeeccef93dd1
nestor <triplek_nes@hotmail.com>
2021-11-20 01:44:40 -0300

    Add project files.

---
+++ Desafio_1/frmPrincipal.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Desafio_1
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtIngresoTexto = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.TextBox();
+            this.lblAutor = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtIngresoTexto);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(489, 56);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingrese una palabra ";
+            // 
+            // txtIngresoTexto
+            // 
+            this.txtIngresoTexto.Location = new System.Drawing.Point(19, 22);
+            this.txtIngresoTexto.Name = "txtIngresoTexto";
+            this.txtIngresoTexto.Size = new System.Drawing.Size(331, 20);
+            this.txtIngresoTexto.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(369, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblResultado);
+            this.panel1.Controls.Add(this.lblCantidad);
+            this.panel1.Location = new System.Drawing.Point(23, 90);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(489, 77);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(45, 25);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(305, 17);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "El máximo valor que puede tener la palabra es:";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(356, 15);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.ReadOnly = true;
+            this.lblCantidad.Size = new System.Drawing.Size(109, 44);
+            this.lblCantidad.TabIndex = 0;
+            this.lblCantidad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblAutor
+            // 
+            this.lblAutor.AutoSize = true;
+            this.lblAutor.Location = new System.Drawing.Point(424, 185);
+            this.lblAutor.Name = "lblAutor";
+            this.lblAutor.Size = new System.Drawing.Size(101, 13);
+            this.lblAutor.TabIndex = 2;
+            this.lblAutor.Text = "Autor: Néstor Oñate";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 221);
+            this.Controls.Add(this.lblAutor);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(550, 260);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(500, 260);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Desafio_1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtIngresoTexto;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox lblCantidad;
+        private System.Windows.Forms.Label lblAutor;
+    }
+}
+
---
+++ Desafio_1/frmPrincipal.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio_1
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String valor = txtIngresoTexto.Text;
+
+            if (valor == "") {
+                lblCantidad.Text = "0";
+                MessageBox.Show("La cadena de texto no debe estar vacía", "Error");
+            }
+            else if (!validarCampos(valor))
+            {
+                lblCantidad.Text = "0";
+                MessageBox.Show("La cadena de texto contiene caracteres no válidos (Se aceptan caracteres en mayúscula del alfabeto inglés)", "Error");
+            }
+            else
+            {
+                List<int> listaCantidad = new List<int>();
+                String letrasUsadas = "";
+                int suma = 0;
+                foreach (char x in valor)
+                {
+                    if (!letrasUsadas.Contains(x))
+                    {
+                        listaCantidad.Add(contarCaracteres(valor, x));
+                        letrasUsadas += x;
+                    }
+                }
+                listaCantidad.Sort();
+                listaCantidad.Reverse();
+                int contadorInicial = 26;
+
+                foreach (int y in listaCantidad)
+                {
+                    suma += y * contadorInicial;
+                    contadorInicial--;
+                }
+
+                lblCantidad.Text = suma.ToString();
+
+            }
+
+        }
+
+
+        private static bool validarCampos(String texto) {
+            char[] alfabeto = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            foreach (char x in texto) 
+                if (!alfabeto.Contains(x)) return false;
+            return true;
+        }
+
+        public static int contarCaracteres(String texto, char letra) {
+            int salida = 0;
+            foreach (char x in texto) {
+                if (x == letra) salida++;
+            }
+            return salida;
+        }
+    }
+}
